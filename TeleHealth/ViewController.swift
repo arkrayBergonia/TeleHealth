016cfb46c0e5f377bd8c2608277cd999bf106987
arkray - Francis Bergonia <fbergonia.arkray@gmail.com>
2020-03-09 14:30:30 +0800

    Initial Commit

---
+++ TeleHealth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TeleHealth
+//
+//  Created by Francis Jemuel Bergonia on 3/9/20.
+//  Copyright © 2020 Xi Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
