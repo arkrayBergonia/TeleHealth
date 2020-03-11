065fb9886c869a1d1b5952f2410b77d7216c3c85
arkray - Francis Bergonia <fbergonia.arkray@gmail.com>
2020-03-11 15:03:57 +0800

    welcom scrn updated

--- TeleHealth/Controllers/WelcomeViewController.swift
+++ TeleHealth/Controllers/WelcomeViewController.swift
@@ -9,15 +9,27 @@
 //
 
 import UIKit
-
-
+import GoogleSignIn
 
 class WelcomeViewController: UIViewController {
 
-   
+    @IBOutlet weak var iconImage: UIImageView!
+    @IBOutlet weak var textfieldStack: UIStackView!
+    @IBOutlet weak var buttonStack: UIStackView!
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    @IBOutlet weak var googleSignInBtn: RoundedButton!
+    @IBOutlet weak var registerBtn: RoundedButton!
+    @IBOutlet weak var loginBtn: RoundedButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.layoutImage()
+        
+        GIDSignIn.sharedInstance()?.presentingViewController = self
+        GIDSignIn.sharedInstance()?.delegate = self as? GIDSignInDelegate
     
     }
     
@@ -25,9 +37,61 @@ class WelcomeViewController: UIViewController {
         super.viewWillAppear(animated)
         self.navigationController?.navigationBar.isHidden = true
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.navigationBar.isHidden = false
     }
     
+    @IBAction func googleSignInBtnPressed(_ sender: RoundedButton) {
+        GIDSignIn.sharedInstance()?.signIn()
+    }
+    
+    @IBAction func registerBtnPressed(_ sender: RoundedButton) {
+        
+    }
+    
+    @IBAction func loginBtnPressed(_ sender: RoundedButton) {
+        self.performSegue(withIdentifier: "goToChat", sender: self)
+    }
+    
+}
+
+
+extension WelcomeViewController {
+    
+    private func layoutImage() {
+        self.iconImage.layer.borderWidth = 1
+        self.iconImage.layer.masksToBounds = false
+        self.iconImage.layer.borderColor = UIColor.systemGreen.cgColor
+        self.iconImage.layer.cornerRadius = self.iconImage.frame.height/2
+        self.iconImage.clipsToBounds = true
+    }
+    
+}
+
+
+extension WelcomeViewController {
+    func signIn(signIn: GIDSignIn!, didSignInForUser user: GIDGoogleUser!,
+                withError error: NSError!) {
+        if (error == nil) {
+            // Perform any operations on signed in user here.
+            self.performSegue(withIdentifier: "goToChat", sender: self)
+            
+            let user: GIDGoogleUser = GIDSignIn.sharedInstance()!.currentUser
+            let fullName = user.profile.name
+            let email = user.profile.email
+            if user.profile.hasImage {
+                let userDP = user.profile.imageURL(withDimension: 200)
+            }
+            print("GoogleUser \(fullName), email:\(email), and has image:\(user.profile.hasImage)")
+            //            self.sampleImageView.sd_setImage(with: userDP, placeholderImage: UIImage(named: "default-profile"))
+            //            } else {
+            //            self.sampleImageView.image = UIImage(named: "default-profile")
+            //            }
+            
+        } else {
+            print("\(error.localizedDescription)")
+        }
+    }
 }
