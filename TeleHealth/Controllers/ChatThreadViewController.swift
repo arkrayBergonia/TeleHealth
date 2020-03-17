380bd1962219bd3eaacea368d9c61d2d80af46f0
Francis Bergonia <49849208+arkrayBergonia@users.noreply.github.com>
2020-03-17 15:22:10 +0800

    ChatThreadViewController added for test

---
+++ TeleHealth/Controllers/ChatThreadViewController.swift
@@ -0,0 +1,86 @@
+//
+//  ChatThreadViewController.swift
+//  TeleHealth
+//
+//  Created by Francis Jemuel Bergonia on 3/17/20.
+//  Copyright © 2020 Xi Apps. All rights reserved.
+//
+
+import UIKit
+import InputBarAccessoryView
+import Firebase
+import MessageKit
+import FirebaseFirestore
+import SDWebImage
+
+class ChatThreadViewController: MessagesViewController {
+var currentUser: User = Auth.auth().currentUser!
+    
+    var user2Name: String?
+    var user2ImgUrl: String?
+    var user2UID: String?
+    
+    private var docReference: DocumentReference?
+    
+    var messages: [Messages] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = user2Name ?? "Chat"
+    }
+    
+}
+
+
+extension ChatThreadViewController: MessagesDataSource {
+    // MARK: - MessagesDataSource
+       func currentSender() -> SenderType {
+           return Sender(id: Auth.auth().currentUser!.uid, displayName: Auth.auth().currentUser?.displayName ?? "Name not found")
+       }
+       
+       func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
+           return messages[indexPath.section]
+       }
+       
+       func numberOfSections(in messagesCollectionView: MessagesCollectionView) -> Int {
+           if messages.count == 0 {
+               print("No messages to display")
+               return 0
+           } else {
+               return messages.count
+           }
+       }
+}
+
+extension ChatThreadViewController: MessagesLayoutDelegate {
+    // MARK: - MessagesLayoutDelegate
+       func avatarSize(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> CGSize {
+           return .zero
+       }
+}
+
+extension ChatThreadViewController: MessagesDisplayDelegate {
+    
+    // MARK: - MessagesDisplayDelegate
+    func backgroundColor(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> UIColor {
+        return isFromCurrentSender(message: message) ? .blue: .lightGray
+    }
+
+    func configureAvatarView(_ avatarView: AvatarView, for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) {
+        
+        if message.sender.senderId == currentUser.uid {
+            SDWebImageManager.shared.loadImage(with: currentUser.photoURL, options: .highPriority, progress: nil) { (image, data, error, cacheType, isFinished, imageUrl) in
+                avatarView.image = image
+            }
+        } else {
+            SDWebImageManager.shared.loadImage(with: URL(string: user2ImgUrl!), options: .highPriority, progress: nil) { (image, data, error, cacheType, isFinished, imageUrl) in
+                avatarView.image = image
+            }
+        }
+    }
+
+    func messageStyle(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageStyle {
+        let corner: MessageStyle.TailCorner = isFromCurrentSender(message: message) ? .bottomRight: .bottomLeft
+        return .bubbleTail(corner, .curved)
+    }
+}
